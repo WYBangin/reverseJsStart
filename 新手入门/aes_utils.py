1a3f23d4882ebbf165b420d9569f6d32822219fb
WYBangin <48352470+WYBangin@users.noreply.github.com>
2022-04-16 17:56:59 +0800

    Update aes_utils.py

--- 新手入门/aes_utils.py
+++ 新手入门/aes_utils.py
@@ -6,9 +6,10 @@ sys.path.append(BASE_DIR)
 
 import base64
 from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
 
 
-class AESTool(object):
+class AESToolCBC(object):
     """AES工具类 这里是CBC模式"""
 
     def __init__(self, key, iv, mode=AES.MODE_CBC):
@@ -36,8 +37,7 @@ class AESTool(object):
     def base64_aes_decrypt(self, data):
         """解密base64格式数据"""
         unpad = lambda s: s[0:-s[-1]]
-        data = data.encode('utf8')  # 转成byte字符串
-        encode_bytes = base64.decodebytes(data)
+        encode_bytes = base64.decodebytes(data.encode('utf8'))
         cipher = AES.new(self.key, self.mode, self.iv)
         text_decrypted = cipher.decrypt(encode_bytes)
         # CBC模式AES加密需要满足加密数据长度是密钥长度的整数倍，所以数据后面可能有不需要的后来添加的数据，所以我们就去掉
@@ -66,9 +66,66 @@ class AESTool(object):
         return text_decrypted
 
 
+class AESToolECB(object):
+    """AES工具类 这里是ECB模式"""
+
+    def __init__(self, key, mode=AES.MODE_ECB):
+        """
+        初始方法
+        :param data: 要加密数据
+        :param key: 秘钥
+        :param iv: 便偏移量
+        :param mode: 加密方式
+        """
+        self.key = key.encode("utf-8")
+        self.mode = mode
+        self.unpad = lambda s: s[0:-ord(s[-1])]  # 截断函数
+
+    def fill_method(self, aes_str):
+        """pkcs7补全函数"""
+        pad_pkcs7 = pad(aes_str.encode('utf-8'), AES.block_size, style='pkcs7')
+        return pad_pkcs7
+
+    def base64_aes_encrypt(self, data):
+        """输出结果为base64"""
+        cipher = AES.new(self.key, self.mode)
+        result = cipher.encrypt(self.fill_method(data))
+        enc_text = str(base64.b64encode(result), encoding="utf-8")
+        return enc_text
+
+    def base64_aes_decrypt(self, data):
+        """解密base64格式数据"""
+        cipher = AES.new(self.key, self.mode)
+        encode_bytes = base64.decodebytes(data.encode('utf8'))
+        text_decrypted = cipher.decrypt(encode_bytes)
+        decrypt_text = self.unpad(text_decrypted.decode('utf8'))
+        return decrypt_text
+
+    def hex_aes_encrypt(self, data):
+        """输出结果为hex格式"""
+        cipher = AES.new(self.key, self.mode)
+        result = cipher.encrypt(self.fill_method(data))
+        enc_text = result.hex()
+        return enc_text
+
+    def hex_aes_decrypt(self, data):
+        """解密hex格式数据"""
+        cipher = AES.new(self.key, self.mode)
+        encode_bytes = bytes.fromhex(data)
+        text_decrypted = cipher.decrypt(encode_bytes)
+        decrypt_text = self.unpad(text_decrypted.decode('utf8'))
+        return decrypt_text
+
+    
 if __name__ == '__main__':
     kk = "20171109124536982017110912453698"
     vv = "2017110912453698"
-    aes_obj = AESTool(kk, vv)
-    print(aes_obj.base64_aes_encrypt("test"))
-    print(aes_obj.hex_aes_encrypt("test"))
+    aes_obj = AESToolCBC(kk, vv)
+    print("CBC加密: ", aes_obj.base64_aes_encrypt("test"))
+    print("CBC解密: ", aes_obj.hex_aes_encrypt("test"))
+    new_key = '1650097882000123'
+    msg = '1234567890abcdefghijklmnopqrstuvwxyz'
+    aes_ecb = AESToolECB(new_key)
+    aes_result = aes_ecb.hex_aes_encrypt(msg)
+    print("ECB加密: ", aes_result)
+    print("ECB解密: ", aes_ecb.hex_aes_decrypt(aes_result))
